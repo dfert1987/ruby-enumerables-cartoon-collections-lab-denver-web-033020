db119cbca3d69779a41e6c73ea09328ac7c2eee4
David Fertitta <davidj.fertitta@gmail.com>
2020-03-22 16:15:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(dwarves)
-dwarves.each_with_index do |dwarves, index|
+dwarves.each_with_index do |name, index|
   puts "#{index +1} #{name}"
 end
 end
